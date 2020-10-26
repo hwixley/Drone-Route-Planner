b91668e74b6a7ee0fa9f76061f00fd73ed5c272a
hwixley <hwixley1@gmail.com>
2020-10-26 19:12:43 +0000

    bug fixing

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -13,6 +13,12 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 import com.mapbox.geojson.*;
 
+import uk.ac.ed.inf.aqmaps.Objects.Point;
+import uk.ac.ed.inf.aqmaps.Objects.Building;
+import uk.ac.ed.inf.aqmaps.Objects.LineGraph;
+import uk.ac.ed.inf.aqmaps.Objects.Sensor;
+import uk.ac.ed.inf.aqmaps.Objects.Building;
+
 public class App 
 {
 	//Confinement area coordinates
@@ -28,28 +34,6 @@ public class App
     //Global variables
     public static ArrayList<Building> buildings = new ArrayList<Building>();
     public static ArrayList<Sensor> sensors = new ArrayList<Sensor>();
-    
-    //OBJECT: lineGraph custom object
-    private static class LineGraph {
-    	Double gradient;
-    	Double yint;
-    	
-    	//Constructor with input
-    	public LineGraph(Point p1, Point p2) {
-    		this.gradient = (p1.lat - p2.lat)/(p1.lng - p2.lng);
-    		this.yint = -gradient*p1.lng + p1.lat;
-    	}
-    	
-    	//Constructor with angle input
-    	public LineGraph(Double angle, Point origin) {
-    		this.gradient = Math.tan(angle);
-    		this.yint = -gradient*origin.lat + origin.lng;
-    	}
-    	
-    	//Default constructor
-    	public LineGraph() {
-    	}
-    }
     
     //METHOD: find point
     private static Point findPoint(Point currPoint, Point nextPoint) {
@@ -224,65 +208,6 @@ public class App
     	}
     	
     	return symbol;
-    }
-    
-    //OBJECT: Custom Sensor object
-    private static class Sensor {
-    	String location;
-    	Double battery;
-    	Double reading;
-    	Point point;
-    	
-    	//Constructor created to clone custom objects effectively
-    	public Sensor(Sensor another) {
-    		this.location = another.location;
-    		this.battery = another.battery;
-    		this.reading = another.reading;
-    		this.point = another.point;
-    	}
-    	
-    	//Constructor with no arguments for default properties
-		public Sensor() {
-		}
-    }
-    
-    //OBJECT: Custom Point object
-    private static class Point {
-    	Double lat = -1.0;
-    	Double lng = -1.0;
-    	
-    	//Constructor created to clone custom objects effectively
-    	public Point(Point another) {
-    		this.lat = another.lat;
-    		this.lng = another.lng;
-    	}
-    	
-    	public Point(Double lat, Double lng) {
-    		this.lat = lat;
-    		this.lng = lng;
-    	}
-    	
-    	//Constructor with no arguments for default properties
-    	public Point() {
-    	}
-    }
-    
-    //OBJECT: Custom building(no-fly-zone) object
-    private static class Building {
-    	ArrayList<Point> points = new ArrayList<Point>();
-    	String name;
-    	String fill;
-    	
-    	//Constructor created to clone custom objects effectively
-    	public Building(Building another) {
-    		this.points = another.points;
-    		this.name = another.name;
-    		this.fill = another.fill;
-    	}
-    	
-    	//Constructor with no arguments for default properties
-    	public Building() {
-    	}
     }
 	
     //METHOD: calculate distance between 2 points
---
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Objects.java
@@ -0,0 +1,87 @@
+package uk.ac.ed.inf.aqmaps;
+
+import java.util.ArrayList;
+
+public class Objects {
+	
+    //OBJECT: Custom Point object
+    public static class Point {
+    	Double lat = -1.0;
+    	Double lng = -1.0;
+    	
+    	//Constructor created to clone custom objects effectively
+    	public Point(Point another) {
+    		this.lat = another.lat;
+    		this.lng = another.lng;
+    	}
+    	
+    	public Point(Double lat, Double lng) {
+    		this.lat = lat;
+    		this.lng = lng;
+    	}
+    	
+    	//Constructor with no arguments for default properties
+    	public Point() {
+    	}
+    }
+    
+    //OBJECT: Custom Sensor object
+    public static class Sensor {
+    	String location;
+    	Double battery;
+    	Double reading;
+    	Point point;
+    	
+    	//Constructor created to clone custom objects effectively
+    	public Sensor(Sensor another) {
+    		this.location = another.location;
+    		this.battery = another.battery;
+    		this.reading = another.reading;
+    		this.point = another.point;
+    	}
+    	
+    	//Constructor with no arguments for default properties
+		public Sensor() {
+		}
+    }
+	
+    //OBJECT: Custom building(no-fly-zone) object
+    public static class Building {
+    	ArrayList<Point> points = new ArrayList<Point>();
+    	String name;
+    	String fill;
+    	
+    	//Constructor created to clone custom objects effectively
+    	public Building(Building another) {
+    		this.points = another.points;
+    		this.name = another.name;
+    		this.fill = another.fill;
+    	}
+    	
+    	//Constructor with no arguments for default properties
+    	public Building() {
+    	}
+    }
+    
+    //OBJECT: lineGraph custom object
+    public static class LineGraph {
+    	Double gradient;
+    	Double yint;
+    	
+    	//Constructor with input
+    	public LineGraph(Point p1, Point p2) {
+    		this.gradient = (p1.lat - p2.lat)/(p1.lng - p2.lng);
+    		this.yint = -gradient*p1.lng + p1.lat;
+    	}
+    	
+    	//Constructor with angle input
+    	public LineGraph(Double angle, Point origin) {
+    		this.gradient = Math.tan(angle);
+    		this.yint = -gradient*origin.lat + origin.lng;
+    	}
+    	
+    	//Default constructor
+    	public LineGraph() {
+    	}
+    }
+}
